8d3abafedf633478c6e1d8865c989a56a7bc0413
Daniele Bertella <daniele.bertella@gmail.com>
2020-11-28 21:44:57 +0000

    Try adding routes in client, not working right now

--- packages/client/src/pages/Game.tsx
+++ packages/client/src/pages/Game.tsx
@@ -1,17 +1,44 @@
-import { Flex, Heading } from 'theme-ui';
+import { Button, Flex, Heading } from 'theme-ui';
 import { SUIT } from 'utils/cardEngine';
 import { range } from 'lodash';
 import { Card } from 'components/Card';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Layout } from 'components/Layout';
+import { useEffect, useState } from 'react';
+import { gameIO } from 'api/socket';
 
 export const Game = () => {
+  const { gameId } = useParams<{ gameId: string }>();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const handleRoom = async (socket: any) => {
+      console.log(socket);
+      await socket.join(gameId);
+      socket.to(gameId).emit('message1', 'Hello server from room' + gameId);
+    };
+    gameIO.on('connection', handleRoom);
+
+    const handleMessage = (arg: string) => {
+      console.log('message2 -', arg);
+      setCount((state) => state + 1);
+    };
+
+    gameIO.on('message2', handleMessage);
+
+    return () => {
+      gameIO.off('connection', handleRoom);
+      gameIO.off('message2', handleMessage);
+    };
+  });
   return (
     <Layout>
       <Flex sx={{ alignItems: 'center', gap: 2 }}>
         <Link to="/">Back</Link>
         <Heading as="h1">Scopa</Heading>
       </Flex>
+      <Button onClick={() => gameIO.emit('message1', 'Hello server')}>Send message to socket {count} times</Button>
+
       <Flex sx={{ m: 3, gap: 3, flexWrap: 'wrap' }}>
         {range(1, 11).map((r) => (
           <Card key={r} card={[r, SUIT.Golds]} />

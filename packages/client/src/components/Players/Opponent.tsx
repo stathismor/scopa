7b5421b83ae7066578c9a66a567016a1de8d0d48
Daniele Bertella <daniele.bertella@gmail.com>
2021-01-09 17:41:02 +0000

    Close #55

--- packages/client/src/components/Players/Opponent.tsx
+++ packages/client/src/components/Players/Opponent.tsx
@@ -7,11 +7,11 @@ export const Opponent = ({ player, ...rest }: { player: PlayerState } & BoxProps
   if (!player) {
     return <Text>Waiting for a player to join</Text>;
   }
-  const { captured, hand } = player;
+  const { captured, hand, scopa } = player;
   return (
     <Box {...rest}>
       <Grid sx={{ m: 3, marginBottom: '-7vw' }} columns="1.5fr 1fr 1fr 1fr">
-        <Deck cardNumber={captured.length} />
+        <Deck cardNumber={captured.length} scopa={scopa} />
         {hand?.map((c) => (
           <Card key={cardKey(c)} card={c} faceDown />
         ))}

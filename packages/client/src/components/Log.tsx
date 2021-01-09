08cc9bae9cef68a2d70fe1a82eb777641e7459da
Daniele Bertella <daniele.bertella@gmail.com>
2021-01-09 12:58:57 +0000

    Adjust border for mobile screens

--- packages/client/src/components/Log.tsx
+++ packages/client/src/components/Log.tsx
@@ -2,7 +2,14 @@ import { Box, Heading, Text } from 'theme-ui';
 
 export const Log = ({ event }: { event: string }) => {
   return (
-    <Box sx={{ borderLeft: '2px solid', borderColor: 'text', pl: 3 }}>
+    <Box
+      sx={{
+        borderLeft: ['none', null, '2px solid'],
+        borderTop: ['2px solid', null, 'none'],
+        borderColor: 'text',
+        p: 3,
+      }}
+    >
       <Heading as="h3" mt={2}>
         Log
       </Heading>

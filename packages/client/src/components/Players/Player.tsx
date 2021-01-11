aaed16572ba561a0393304a7b28cb567e4504961
Stathis <emoraitidis@gmail.com>
2021-01-11 21:01:17 +0000

    Face down cards on spectator

--- packages/client/src/components/Players/Player.tsx
+++ packages/client/src/components/Players/Player.tsx
@@ -43,7 +43,7 @@ export const Player = ({
               sx={playerCardWrapper(activePlayerCard === key)}
               onClick={isActive ? () => togglePlayerActiveCard((state) => (state === key ? null : key)) : noop}
             >
-              <Card card={c} />
+              <Card card={c} faceDown={isSpectator} />
             </CardWrapper>
           );
         })}

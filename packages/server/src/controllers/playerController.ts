1a760a6712cb149b92706036b9707a6dc3d35126
Daniele Bertella <daniele.bertella@gmail.com>
2021-01-24 22:14:19 +0000

    Add created and updated at into schemas, sort rooms by updatedAt (#71)

--- packages/server/src/controllers/playerController.ts
+++ packages/server/src/controllers/playerController.ts
@@ -1,12 +1,10 @@
 import { Player, IPlayer } from '../database/models';
 
 export async function getPlayer(name: string): Promise<IPlayer | null> {
-  const player = await Player.findOne({ name });
-  return player;
+  return Player.findOne({ name });
 }
 
 export async function createPlayer(name: string): Promise<IPlayer> {
   const player = new Player({ name });
-  await player.save();
-  return player;
+  return player.save();
 }
